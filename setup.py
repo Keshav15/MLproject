8345d2dac1cfec6f5c163abb8097067299f44ba7
keshavbajaj4444@gmail.com <keshavbajaj4444@gmail.com>
2023-04-01 22:16:30 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name="mlproject",
+version="0.0.1",
+author="Keshav",
+author_email="keshavbajaj4444@gmail.com",
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
